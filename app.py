6cdcb83b77dcf62dac929d583967254dbc7a4176
Atharva Malode <Atharvamalode21@gmail.com>
2023-05-23 16:45:21 +0530

    bug : categorial value not fixed yet

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+def Mushroom_Predictor(result_data):
+    prediction = np.array(result_data).reshape(1, 5)
+    load_model = pickle.load(open('decision_tree_model.pkl', 'rb'))
+    result = load_model.predict(prediction)
+    return result[0]
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-   return render_template('index.html')
+def home():
+    return render_template('index.html')
 
-@app.route('/Predict', methods=['POST'])
-def Predict():
-    return render_template('about.html')
+@app.route('/predict', methods=['POST'])
+def predict():
+    result_data = request.form.to_dict()
+    result = Mushroom_Predictor(list(result_data.values()))
+    if int(result) == 1:
+        prediction = 'Edible'
+    else:
+        prediction = 'Poisonous'
+    return render_template('result.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
